9718d776d0972b523880004fc008fc78a98f7c66
MicyEK <115043168+MicyEK@users.noreply.github.com>
2023-10-29 15:40:51 -0500

    Random updates

---
+++ 404.js
@@ -0,0 +1,12 @@
+window.onload = function exampleFunction() {
+    let f = Math.floor(Math.random() * 4) + 1;
+    if (f === 1) {
+        document.querySelector("h1").innerHTML=".(-.-)."
+    } else if (f === 2) {
+        document.querySelector("h1").innerHTML="(>.<)"
+    } else if (f === 3) {
+        document.querySelector("h1").innerHTML="-(._.)-"
+    } else if (f === 4) {
+        document.querySelector("h1").innerHTML="(-^_^-)"
+    } 
+} 
